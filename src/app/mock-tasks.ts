304f0034acbd0afe8b2055e33c1413977240f437
Siddhesh Kankekar <kankekas@tcd.ie>
2021-10-24 20:30:42 +0100

    Initiate ToDo components+Fakejson+services+UiElements

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,34 @@
+import {Task} from "./Task"
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Good Meal',
+    day: 'Early Morning',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Ace in the Interview',
+    day: 'Whenever',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Prepare for the Interviews',
+    day: 'Always',
+    reminder: true,
+  },
+  {
+    id: 4,
+    text: 'Go to Workout',
+    day: 'Afternoon',
+    reminder: true,
+  },
+  {
+    id: 5,
+    text: 'Go for Grocery',
+    day: 'Evening',
+    reminder: false,
+  },
+];
